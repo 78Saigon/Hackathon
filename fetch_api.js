62576a9d956676c12aad257f7f47495abd568914
78Saigon <dangnam.nguyen@lilo.org>
2025-11-16 23:17:05 +0100

    1st commit

---
+++ fetch_api.js
@@ -0,0 +1,19 @@
+﻿fetch("http://localhost:8080/participants", {
+    method: "POST",
+    body: JSON.stringify({
+        userId: 1,
+        title: "Participants",
+        name: LastName,
+        first_name: FirstName, 
+        mail: Mail, 
+        role: Role, 
+        competences: Competence,
+        completed: false
+    }),
+    headers: {
+        "Content-type": "application/json; charset=UTF-8"
+    }
+})
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+    catch (error => console.error('Error', error));
